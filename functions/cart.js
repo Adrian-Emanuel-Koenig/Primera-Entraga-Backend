cfe6064c73b72cf598dc1c5b36d9a74eec36bfe7
Adrián Emanuel Koenig <adrian.95.koenig@gmail.com>
2022-11-25 22:43:30 -0300

    postman

--- functions/cart.js
+++ functions/cart.js
@@ -11,63 +11,60 @@ const allCarts = async (req, res) => {
 };
 
 const postCart = async (req, res) => {
-    try {
-        let timestamp = new Date().toLocaleString();
-        const { body } = req;
-        carrito.save({ productos: body, timestamp });
-        res.json(await carrito.getAll());
-    } catch (error) {
-        res.json(error)
-    }
-
+  try {
+    let timestamp = new Date().toLocaleString();
+    const { body } = req;
+    carrito.save({ productos: body, timestamp });
+    res.json(await carrito.getAll());
+  } catch (error) {
+    res.json(error);
+  }
 };
 
 const getCart = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const producto = await carrito.getById(id);
-        const item = producto.productos;
-        res.json(item);
-    } catch (error) {
-        res.json(error)
-    }
-
+  try {
+    const { id } = req.params;
+    const producto = await carrito.getById(id);
+    const item = producto.productos;
+    res.json(item);
+  } catch (error) {
+    res.json(error);
+  }
 };
 
 const deleteCart = async (req, res) => {
-    try {
-        const { id } = req.params;
-        deleteCart = await carrito.deleteById(id);
-        res.json(deleteCart);
-    } catch (error) {
-        res.json(error)
-    }
-
+  try {
+    const carros = await carrito.getAll()
+    const { id } = req.params;
+    deleteCart = await carrito.deleteById(id);
+    res.json(carros);
+  } catch (error) {
+    res.json(error);
+  }
 };
 
 const postProductToCart = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const { body } = req;
-        const producto = await carrito.getById(id);
-        const item = producto.productos;
-        item.push(body);
-        res.json(item);
-    } catch (error) {
-        res.json(error);
-    }
-
+  try {
+    const { id } = req.params;
+    const { body } = req;
+    const producto = await carrito.getById(id);
+    const item = producto.productos;
+    item.push(body);
+    res.json(item);
+  } catch (error) {
+    res.json(error);
+  }
 };
 
 const deleteCartProduct = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const { id_prod } = req.params;
-        await carrito.deleteItem((id), (id_prod));
-        res.json("Producto eliminado con éxito.")
-    } catch(error) {
-        res.json(error);
-    }
+  try {
+    const { id } = req.params;
+    const { id_prod } = req.params;
+    await carrito.deleteItem(id, id_prod);
+    res.json("Producto eliminado con éxito.");
+  } catch (error) {
+    res.json(error);
+  }
 };
 
 module.exports = {
